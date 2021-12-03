67f1b9cc5134df7104ee15f779fc51dfdc588a1b
Suliman Fayyaz <hydrajin@gmail.com>
2021-12-02 23:37:40 -0500

    First commit

---
+++ breedFetcher.js
@@ -0,0 +1,11 @@
+const request = require('request');
+
+const fetchBreedDescription = (inputBreed, callback) => {
+request('http://example.edu', (error, response, body) => {
+
+
+//   console.log('error:', error); // Print the error if one occurred
+//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//   console.log('body:', body); // Print the HTML for the Google homepage.
+ });
+};
