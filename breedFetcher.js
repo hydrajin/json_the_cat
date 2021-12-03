09be710898defe94e37aa9b37fadc270294c6f60
Suliman Fayyaz <hydrajin@gmail.com>
2021-12-02 23:56:26 -0500

    gets Siberian array search param

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,11 +1,16 @@
 const request = require('request');
 
-const fetchBreedDescription = (inputBreed, callback) => {
-request('http://example.edu', (error, response, body) => {
+// const fetchBreedDescription = (inputBreed, callback) => {
+  request('https://api.thecatapi.com/v1/breeds/search?q=Siberian', (error, response, body) => {
+    console.log('statusCode:', response && response.statusCode);
+  if (error) {
+      console.log('error:', error); // Print the error if one occurred
+      return;
+    } else {
+      console.log('body:', body); // Print the HTML for thecatapi
+    }
 
+    //   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
 
-//   console.log('error:', error); // Print the error if one occurred
-//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-//   console.log('body:', body); // Print the HTML for the Google homepage.
- });
-};
+  });
+// };
