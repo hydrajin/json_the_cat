d5602624ea5e91181342ed7085be56d766030551
Suliman Fayyaz <hydrajin@gmail.com>
2021-12-03 00:39:22 -0500

    Added terminal input for breed and it worked

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,25 @@
+const args = process.argv.slice(2);
+const inputBreed = args[0];
+
 const request = require('request');
 
-// const fetchBreedDescription = (inputBreed, callback) => {
-  request('https://api.thecatapi.com/v1/breeds/search?q=Siberian', (error, response, body) => {
+const fetchBreedDescription = (inputBreed, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${inputBreed}`, (error, response, body) => {
     console.log('statusCode:', response && response.statusCode);
-  if (error) {
+    const data = JSON.parse(body); // ADDED to make string into OBJECT!
+    console.log(data);
+    //
+    // console.log('type of body:', typeof body); // string!
+    if (error) {
       console.log('error:', error); // Print the error if one occurred
       return;
     } else {
-      console.log('body:', body); // Print the HTML for thecatapi
+      // console.log('body:', body); // Print the HTML for thecatapi
     }
 
     //   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
 
   });
-// };
+};
+
+fetchBreedDescription(inputBreed); // IT WORKED!!!
